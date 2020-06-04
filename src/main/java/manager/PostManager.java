5273d2af4e987cbbed4e750e6ce833ebcfcb9bdb
Anatoly M <m@m.com>
2020-06-05 00:19:48 +0300

    Task 2 "Manager"

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,19 @@
+package manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] getPosts() {
+        return null;
+    }
+
+    public Post[] search(int postId){
+        return null;
+    }
+
+    public void delete(int postId){
+
+    }
+}
